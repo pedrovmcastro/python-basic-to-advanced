67790279e66bcfae46c13fecc27ef44c5e6790c6
Pedro Castro <pvmcastro@gmail.com>
2024-02-15 17:32:12 -0300

    files

--- 09_files/9-01_introduction.py
+++ 09_files/9-01_introduction.py
@@ -1,7 +1,7 @@
 """
 Exercise 1:
 Write a programam that:
-(a) Creates/opens a text file named "arq.txt";
+(a) Creates/opens a text file named "introduction.txt";
 (b) Allows the user to record various characteres in this file until the user enters the character '0';
 (c) Closes the file.
 
@@ -9,15 +9,15 @@ Write a programam that:
 (PT-BR)
 Exercício 1:
 Escreva um programa que:
-(a) Crie/abra um arquivo texto de nome "arq.txt"
+(a) Crie/abra um arquivo texto de nome "introduction.txt"
 (b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o usuário entre com o caractere '0'
 (c) Feche o arquivo
 """
 
-with open('arq.txt', 'w') as arq:   # "with" automatically closes the file. 
+with open('introduction.txt', 'w') as file:   # "with" automatically closes the file. 
     while True:
         char = input()
         if char != '0':
-            arq.write(char)
+            file.write(char)
         else:
             break
